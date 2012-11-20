803d81b8f013f0f317efcdf555aeddce950887d9
HackFisher <wxiaoyin@gmail.com>
2012-11-20 13:57:48 +0800

    face fix

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ weixin
 ======
 
 微信公众平台
+
+Prepare: sudo apt-get install php-curl
+
+php face.php
--- face.php
+++ face.php
@@ -7,7 +7,6 @@ $api_key = "e336859e1099669c6662a3ca76c590b8";
 $api_secret = "HcMo_lQFQ4RZ1qrijxjjnzoHeNjxMc18";
 // initialize client object
 $api = new FacePPClient($api_key, $api_secret);
-
 // the list of person_name to train and recognize for
 $person_names = array("denny", "beauty");
 // store the face_ids obtained by detection/detect API
@@ -37,7 +36,6 @@ function detect(&$api, $person_name, &$face_ids)
     
     // detect faces in this photo
     $result = $api->face_detect($url);
-	echo $result;
     // skip errors
     if (empty($result->face))
         return false;
