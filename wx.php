a7d6fd7fcb8efd80989529aa5ccb5a730a952552
HackFisher <wxiaoyin@gmail.com>
2012-11-22 14:33:22 +0800

    map feature

--- wx.php
+++ wx.php
@@ -34,26 +34,56 @@ class wechatCallbackapiTest
               	$postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
                 $fromUsername = $postObj->FromUserName;
                 $toUsername = $postObj->ToUserName;
-                $keyword = trim($postObj->Content);
-                $time = time();
-                $textTpl = "<xml>
-							<ToUserName><![CDATA[%s]]></ToUserName>
-							<FromUserName><![CDATA[%s]]></FromUserName>
-							<CreateTime>%s</CreateTime>
-							<MsgType><![CDATA[%s]]></MsgType>
-							<Content><![CDATA[%s]]></Content>
-							<FuncFlag>0</FuncFlag>
-							</xml>";             
-				if(!empty( $keyword ))
-                {
-              		$msgType = "text";
-                	$contentStr = "Hi, I'm Mars Robot! 有何贵干?";
-                	$resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
-                	echo $resultStr;
-                }else{
-                	echo "Input something...";
-                }
-
+				$msgType = $postObj->MsgType;
+				
+				if ($msgType=="text") {
+					$keyword = trim($postObj->Content);
+					$time = time();
+					$textTpl = "<xml>
+								<ToUserName><![CDATA[%s]]></ToUserName>
+								<FromUserName><![CDATA[%s]]></FromUserName>
+								<CreateTime>%s</CreateTime>
+								<MsgType><![CDATA[%s]]></MsgType>
+								<Content><![CDATA[%s]]></Content>
+								<FuncFlag>0</FuncFlag>
+								</xml>";             
+					if(!empty( $keyword ))
+					{
+						$msgType = "text";
+						$contentStr = "Hi, I'm Mars Robot! 有何贵干?";
+						$resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+						echo $resultStr;
+					}else{
+						echo "Input something...";
+					}
+				} else if ($msgType=="location"){
+					$location_x = $postObj->Location_X;
+					$location_y = $postObj->Location_Y;
+					$scale = $postObj->Scale;
+					$label = $postObj->Label;
+					$time = time();
+					$textTpl = "<xml>
+								<ToUserName><![CDATA[%s]]></ToUserName>
+								<FromUserName><![CDATA[%s]]></FromUserName>
+								<CreateTime>%s</CreateTime>
+								<MsgType><![CDATA[%s]]></MsgType>
+								<Content><![CDATA[%s]]></Content>
+								<FuncFlag>0</FuncFlag>
+								</xml>";
+					require_once(__DIR__ . "/BaiduMapClient.php");
+					$api_key = "0e4fde2b4acbc043abdb68df511359ae";
+					// initialize client object
+					$api = new BaiduMapClient($api_key);
+					$result = $api->geocoder_location($location_x.",".$location_y);
+					if (!empty($result)) {
+						$msgType = "text";
+						$resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $result);
+						echo $resultStr;
+					}
+				} else {
+					echo "";
+					exit;
+				}
         }else {
         	echo "";
         	exit;
