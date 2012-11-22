7dc33ee118399096f0c046a12a2ef0965c0c365b
HackFisher <wxiaoyin@gmail.com>
2012-11-22 11:20:41 +0800

    location error

--- map.php
+++ map.php
@@ -5,7 +5,7 @@ $api_key = "0e4fde2b4acbc043abdb68df511359ae";
 // initialize client object
 $api = new BaiduMapClient($api_key);
 
-$api->place_search("百度大厦", "39.915,116.404,39.975,116.414", "2000");
+$api->place_search("百度大厦", "39.915,116.404", "2000");
 
 $api->geocoder_address("百度大厦");
 
