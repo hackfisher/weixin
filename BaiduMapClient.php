efed6b5c793df326a7c2638e92849ebab59b838d
HackFisher <wxiaoyin@gmail.com>
2012-11-22 16:59:00 +0800

    aa

--- BaiduMapClient.php
+++ BaiduMapClient.php
@@ -1,5 +1,4 @@
 ﻿<?php
-header("Content-type: text/html; charset=utf-8");
 define ("DEBUG_MODE", false);
 
 class BaiduMapClient
--- map.php
+++ map.php
@@ -1,4 +1,5 @@
 ﻿<?php
+header("Content-type: text/html; charset=utf-8");
 require_once(__DIR__ . "/BaiduMapClient.php");
 
 $api_key = "0e4fde2b4acbc043abdb68df511359ae";
